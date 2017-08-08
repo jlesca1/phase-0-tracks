8064a8fa6c3b4101aee1be9d5267fab96a83fb4e
Joseph Lesca <josephlesca@hotmail.com>
2017-08-08 12:26:28 -0700

    how to eat cereal

---
+++ ruby/eat_cereal.rb
@@ -0,0 +1,13 @@
+# Walk in kitchen
+# Walk to table and pick up box of cereal
+# Walk to cabinites open draw, get spoon, and close the drawer
+# Open cabinate, get bowl, close cabinite
+# Walk to frig, open door, get milk, close door
+# Walk to tv room, sit on couch
+# Put bowl on ottoman
+# Put spoon, in bowl
+# Put a cup of cerial in bowl
+# Add milk to bowl so that milk is at same level as cereal
+# Turn on tv
+# Eat cereal
+# If still hungry repeat steps 9, 10, and 12 as needed
