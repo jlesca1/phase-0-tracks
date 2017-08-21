2e497b3ea5fbd8e1fcd3441ec3a4e48ecb140814
Joseph Lesca <josephlesca@hotmail.com>
2017-08-20 22:45:10 -0700

    printing correct alias

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,36 +4,55 @@
 # - Swap first and last name
 # - Chanage each vowel to next vowel
 # - Change each consonant to next consonant
-# -
+# - Print out Alias
 
 # Main Code
 
 print "Please enter your first and last name: "
-name = gets.chomp
+name = gets.chomp.downcase
 
-name_array = name.split(" ")
-first_name = name_array.shift
-name_array.push(first_name)
+def alias(name)
+  name_array = name.split(" ")
+  first_name = name_array.shift
+  name_array.push(first_name)
 
-new_name = name_array[0] + " " + name_array[1]
+  new_name = name_array[0] + " " + name_array[1]
 
-letters_array = new_name.split("")
-p "letters array"
-p letters_array
+  letters_array = new_name.split("")
 
-next_index = 0
-final_name_array = []
-vowel_array = ["a","e","i","o","u"]
-consonant_array = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"]
+  next_index = 0
+  final_name_array = []
+  vowel_array = ["a","e","i","o","u"]
+  consonant_array = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y","z"]
 
-# This will itterate through the letters_array array and switch each vowel to the next vowel and each consonant to the next consonant
-letters_array.map! do |letter|
-  if vowel_array.include?(letter)
-    index = vowel_array.index(letter)
-    next_index = index + 1
-    new_letter = vowel_array[next_index]
-    final_name_array.push(new_letter)
-    p "New letter #{new_letter}"
+  # This will iterate through the letters_array array and switch each vowel to the next vowel and each consonant to the next consonant
+
+  letters_array.map! do |letter|
+    if vowel_array.include?(letter)
+      index = vowel_array.index(letter)
+
+      if index < vowel_array.length-1
+        next_index = index + 1
+        new_letter = vowel_array[next_index]
+        final_name_array.push(new_letter)
+      else
+        new_letter = vowel_array[0]
+        final_name_array.push(new_letter)
+      end
+    elsif consonant_array.include?(letter)
+      index = consonant_array.index(letter)
+
+      if index < vowel_array.length-1
+        next_index = index + 1
+        new_letter = consonant_array[next_index]
+        final_name_array.push(new_letter)
+      else
+        new_letter = consonant_array[0]
+        final_name_array.push(new_letter)
+      end
+    else
+      final_name_array.push(" ")
+    end
+    p final_name = final_name_array.join("")
   end
-  #p "Final name #{final_name_array}"
 end
