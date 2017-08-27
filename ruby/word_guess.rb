ac4b2f99e6d1b5f9d6fc077c5169e9868ebb2837
Joseph Lesca <josephlesca@hotmail.com>
2017-08-26 17:18:01 -0700

    Takes in word and guess and lets users konw if they gussed correctly.  Next needs to modify result arry with correctly gussed letters.

--- ruby/word_guess.rb
+++ ruby/word_guess.rb
@@ -11,9 +11,46 @@
 # If word is gussed, user gets a contrats message
 # If the word is not guess the get a message letting them know they did not guess the word
 
+
 # Main Code #############################################
 
+word_array=[]
+result_array=[]
+
+class Number_guessing
+  attr_reader :letter_guess
+  attr_accessor :word_array, :word
+
+  def initialize
+    @correct_guess = false
+    @guess_count
+    @letter_guess
+    @word
+  end
+
+  def guess(word_array, letter_guess)
+    if word_array.include?(letter_guess)
+      print "Good job! The word contains #{letter_guess}"
+      # print the word with letters guessed so far
+    else
+      puts "Sorry, the word does not contain the letter #{letter_guess}"
+    end
+  end
+
+end
 
 
 # Driver Code ###########################################
 
+print "Input a word for the other player to guess: "
+p word = gets.chomp
+p word_array = word.split(%r{\s*})
+result_array = word.split(%r{\s*})
+p result_array.fill("-")
+
+print "Guess a letter: "
+p letter_guess = gets.chomp
+
+number_guessing = Number_guessing.new()
+
+number_guessing.guess(word_array, letter_guess)
